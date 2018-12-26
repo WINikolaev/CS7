d487bee848cc82e234812b8143bd98901f5afd0c
Igor <40776860+WINikolaev@users.noreply.github.com>
2018-12-26 17:15:33 +0300

    no message

---
+++ Chapter06/PacktLibrary/PacktLibrary/Person.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace Packt.CS7
+{
+    public partial class Person : IComparable<Person>
+    {
+        // событие
+        public event EventHandler Shout;
+        // поле
+        public int AngerLevel;
+
+        // метод
+        public void Poke()
+        {
+            AngerLevel++;
+            if (AngerLevel >= 3)
+            {
+                // если кто-то нас слушает...
+                if (Shout != null)
+                {
+                    // ...поднимается событие
+                    Shout(this, EventArgs.Empty);
+                }
+            }
+        }
+
+        // поля
+        public string Name;
+        public DateTime DateOfBirth;
+        public List<Person> Children = new List<Person>();
+        // методы
+        public void WriteToConsole()
+        {
+            WriteLine($"{Name} was born on {DateOfBirth:dddd, d MMMM yyyy}");
+        }
+
+        // методы "размножения"
+        public static Person Procreate(Person p1, Person p2)
+        {
+            var baby = new Person
+            {
+                Name = $"Baby of {p1.Name} and {p2.Name}"
+            };
+            p1.Children.Add(baby);
+            p2.Children.Add(baby);
+            return baby;
+        }
+        public Person ProcreateWith(Person partner)
+        {
+            return Procreate(this, partner);
+        }
+
+        // оператор для «умножения»
+        public static Person operator *(Person p1, Person p2)
+        {
+            return Person.Procreate(p1, p2);
+        }
+
+        // метод с локальной функцией
+        public static int Factorial(int number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentException(
+                $"{nameof(number)} cannot be less than zero.");
+            }
+            return localFactorial(number);
+            int localFactorial(int localNumber)
+            {
+                if (localNumber < 1) return 1;
+                return localNumber * localFactorial(localNumber - 1);
+            }
+        }
+        ////////////////////////////
+         public int CompareTo(Person other)
+         {
+            return Name.CompareTo(other.Name);
+         }
+        
+        //////////////////////////////
+    }
+}
---
+++ Chapter06/PacktLibrary/PeopleApp/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Packt.CS7;
+using static System.Console;
+
+namespace PeopleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var harry = new Person { Name = "Harry" };
+            var mary = new Person { Name = "Mary" };
+            var jill = new Person { Name = "Jill" };
+
+            // метод вызова экземпляра
+            var baby1 = mary.ProcreateWith(harry);
+            // метод статического вызова
+            var baby2 = Person.Procreate(harry, jill);
+
+            WriteLine($"{mary.Name} has {mary.Children.Count} children.");
+            WriteLine($"{harry.Name} has {harry.Children.Count} children.");
+            WriteLine($"{jill.Name} has {jill.Children.Count} children.");
+            WriteLine($"{mary.Name}'s first child is named\"{mary.Children[0].Name}\".");
+
+            WriteLine("After Operator\r\n");
+
+            var baby3 = harry * mary;
+
+            WriteLine($"{mary.Name} has {mary.Children.Count} children.");
+            WriteLine($"{harry.Name} has {harry.Children.Count} children.");
+            WriteLine($"{jill.Name} has {jill.Children.Count} children.");
+            WriteLine($"{mary.Name}'s first child is named\"{mary.Children[0].Name}\".");
+
+            WriteLine("Factorial\r\n");
+
+            WriteLine($"5! is {Person.Factorial(5)}");
+
+            WriteLine("Events\r\n");
+
+            harry.Shout += Harry_Shout;
+            harry.Poke();
+            harry.Poke();
+            harry.Poke();
+            harry.Poke();
+
+            WriteLine("Interface\r\n");
+            Person[] people = {
+                new Person { Name = "Simon" },
+                new Person { Name = "Jenny" },
+                new Person { Name = "Adam" },
+                new Person { Name = "Richard" }
+            };
+
+            WriteLine("Initial list of people:");
+            foreach (var person in people)
+            {
+                WriteLine($"{person.Name}");
+            }
+            WriteLine("Use Person's IComparable implementation to sort:");
+            Array.Sort(people);
+            foreach (var person in people)
+            {
+                WriteLine($"{person.Name}");
+            }
+
+            WriteLine("Use PersonComparer's IComparer implementation to sort:");
+            Array.Sort(people, new PersonComparer());
+            foreach (var person in people)
+            {
+                WriteLine($"{person.Name}");
+            }
+
+            var t = new Thing();
+            t.Data = 42;
+            WriteLine($"Thing: {t.Process("42")}");
+
+            var gt = new GenericThing<int>();
+            gt.Data = 42;
+            WriteLine($"GenericThing: {gt.Process("42")}");
+
+            string number1 = "4";
+            WriteLine($"{number1} squared is {Squarer.Square<string>(number1)}");
+            byte number2 = 3;
+            WriteLine($"{number2} squared is {Squarer.Square<byte>(number2)}");
+
+            ReadLine();
+        }
+
+
+        private static void Harry_Shout(object sender, EventArgs e)
+        {
+            Person p = (Person)sender;
+            WriteLine($"{p.Name} is this angry: {p.AngerLevel}.");
+        }
+    }
+}
